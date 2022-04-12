504c95b0b714a3ebb338e3725aba13162c0db922
unknown <you@example.com>
2022-04-12 10:35:26 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+;
+
+
+const app = {
+    song: [
+       {
+           name: "Em ơi đưng sầu",
+           singer: "NB3 Hoài Bảo",
+           path: "./music/emoidungsau.mp3",
+           image: "./img/emoidungsau.jpg"
+       },
+       {
+           name: "Mộng mơ",
+           singer: "Masew",
+           path: "./music/mongmo.mp3",
+           image: "./img/mongmo.jpg"
+       },
+       {
+           name: "Chỉ muốn bên em thật gần",
+           singer: "Yling Lạc",
+           path: "./music/nhungcaucadiuem.mp3",
+           image: "./img/nhungcaucadiuem.jpg"
+       },
+       {
+           name: "Vài lần đón đưa",
+           singer: "Soobin Hoàng Sơn",
+           path: "./music/vailandondua.mp3",
+           image: "./img/vailandondua.jpg"
+       }
+   ]
+       
+
+}
